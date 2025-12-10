4166122d3c2e36c05f4fba4f3d5dd0c37e0a3c54
hohokz <hohokz@hotmail.com>
2025-12-10 08:32:24 +0700

    fix : email service

--- utils/emailService.js
+++ utils/emailService.js
@@ -5,8 +5,8 @@ const transporter = nodemailer.createTransport({
   port: Number(process.env.EMAIL_PORT),
   secure: false,
   auth: {
-    user: process.env.EMAIL_USER, // apikey
-    pass: process.env.EMAIL_PASS, // xsmtpsib-xxxxx
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
