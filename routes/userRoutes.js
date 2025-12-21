398c75562d3388cd39b448794a961cb8d25f0f52
hohokz <hohokz@hotmail.com>
2025-12-21 19:20:12 +0700

    add : user CRUD api

---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+const { authenticateToken, authorizeRole } = require('../middlewares/authMiddleware');
+
+// All routes require authentication and ADMIN role
+router.use(authenticateToken);
+router.use(authorizeRole(['ADMIN']));
+
+// CRUD Routes
+router.get('/', userController.getUsers);
+router.get('/:id', userController.getUser);
+router.post('/', userController.createUser);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
+
+module.exports = router;
